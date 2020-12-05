0d4e20ccc74e4d64bd2a26acd119295e84901057
khicken <kalebkim1939@gmail.com>
2020-12-04 23:34:30 -0800

    updated working code, configured for schoology site

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
-chrome.browserAction.onClicked.addListener(function() {
-	chrome.tabs.query({
-		currentWindow: true,
-		active: true
-	}, function(tab) {
-		chrome.tabs.create({
-			"url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-		});
-	});
-});
+// just kept it simple lol
+document.getElementsByClassName("overdue-submissions")[0].remove();
